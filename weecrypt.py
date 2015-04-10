a9ce8fb3e06a3c476043dae519c41bd1fb0f4f3e
fro_ozen <fro_ozen@gmx.de>
2015-04-10 10:50:36 +0200

    Implement basic GPG en- and decryption

--- weecrypt.py
+++ weecrypt.py
@@ -6,15 +6,27 @@ import subprocess
 weechat.register("weecrypt", "shak-mar", "0.1", "None",
                  "asymmetric encryption for weechat using gpg", "", "")
 
+# Only the traffic on whitelisted channels will be en- and decrypted
 channel_whitelist = ["#yourchannel"]
 in_stream = False
 stream_buffer = ""
 
+# GPG Identifiers for the people whose keys you own.
+# A GPG Identifier is something that is unique to the public key you wish to
+# use.
+gpg_identifiers = {
+        "nickname": "gpg_identifier"
+        }
 
+max_length = 300
+
+
+# Retrieve your own nick
 def my_nick(server_name):
     return weechat.info_get("irc_nick", server_name)
 
 
+# Returns all nicks in a channel, except your own
 def other_nicks(channel_name, server_name):
     nicks = []
     infolist = weechat.infolist_get("irc_nick", "",
@@ -29,22 +41,33 @@ def other_nicks(channel_name, server_name):
     return nicks
 
 
-def encrypt(message, to_nick):
-    p = subprocess.Popen(["base64", "-w", "0"],
+# Encrypt a message for all possible recipients
+def encrypt(message, to_nicks):
+    # Assemble the command
+    command = ["gpg2", "--armor", "--encrypt"]
+    for nick in to_nicks:
+        if nick in gpg_identifiers:
+            command.extend(["--recipient", gpg_identifiers[nick]])
+
+    # Run the command and collect its output
+    p = subprocess.Popen(command,
                          stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     encoded, _ = p.communicate(message.encode())
     encoded = encoded.decode().strip()
     return encoded
 
 
+# Decrypt a received message
 def decrypt(message):
-    p = subprocess.Popen(["base64", "-d", "-w", "0"],
-                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    p = subprocess.Popen(["gpg2", "--armor", "--decrypt"],
+                         stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
     decoded, _ = p.communicate(message.encode())
     decoded = decoded.decode().strip()
     return decoded
 
 
+# Parse an IRC message into a useable format
 def parse_message(irc_message, server=None):
     # parse the message
     message_dict = {"message": irc_message}
@@ -56,12 +79,14 @@ def parse_message(irc_message, server=None):
     return(parsed, message)
 
 
+# This method modifies how received IRC messages are displayed
 def in_modifier(data, modifier, server_name, irc_message):
     global in_stream
     global stream_buffer
 
     parsed, message = parse_message(irc_message, server=server_name)
 
+    # Only decrypt on whitelisted channels
     if parsed["channel"] not in channel_whitelist:
         return irc_message
 
@@ -69,6 +94,7 @@ def in_modifier(data, modifier, server_name, irc_message):
         return ":%s PRIVMSG %s :%s" % \
                (parsed["host"], parsed["channel"], message)
 
+    # Start buffering
     if message.startswith("crypt:"):
         in_stream = True
         stream_buffer = ""
@@ -76,14 +102,19 @@ def in_modifier(data, modifier, server_name, irc_message):
     if in_stream:
         stream_buffer += message
 
+    # Finished buffering: decrypt the message
     if in_stream and message.endswith(":crypt"):
         in_stream = False
-        split = stream_buffer.split(":")
-        l = len(split)
-        for i in range(1, l - 2, 2):
-            if split[i] == my_nick(server_name):
-                return build_message(decrypt(split[i + 1]))
 
+        # Turn the message into the original ASCII armor
+        split = stream_buffer.split(":")
+        message = ":".join(split[1:-1])
+        # Put the newlines back, as GPG needs them
+        message = message.replace("\\n", "\n", -1)
+
+        return build_message(decrypt(message))
+
+    # Don't print anything while buffering
     if in_stream:
         return ""
 
@@ -92,28 +123,34 @@ def in_modifier(data, modifier, server_name, irc_message):
 weechat.hook_modifier("irc_in2_privmsg", "in_modifier", "")
 
 
+# This method modifies how IRC messages are sent
 def out_modifier(data, modifier, server_name, irc_message):
     parsed, message = parse_message(irc_message, server=server_name)
 
+    # Only encrypt on whitelisted channels
     if parsed["channel"] not in channel_whitelist:
         return irc_message
 
-    new_message = "crypt:"
-    for nick in other_nicks(parsed["channel"], server_name):
-        new_message += nick + ":" + encrypt(message, nick) + ":"
+    receipients = other_nicks(parsed["channel"], server_name)
+    new_message = "crypt:%s:crypt" % encrypt(message, receipients)
+    # Encode the newlines, as they are not allowed by the IRC protocol
+    new_message = new_message.replace("\n", "\\n", -1)
 
-    def build_message(add_end=True):
-        message = "PRIVMSG %s :%s" % (parsed["channel"], new_message)
-        if add_end:
-            message += "crypt"
-        return message
+    def build_message(message):
+        return "PRIVMSG %s :%s" % (parsed["channel"], message)
 
-    last_irc_message = build_message()
-    send_irc_message = last_irc_message
-    while len(last_irc_message) > 512:
-        new_message = last_irc_message[512:]
-        last_irc_message = build_message(False)
-        send_irc_message += "\n" + last_irc_message
-    return send_irc_message
+    # The message has to be split into multiple messages, as ASCII armors
+    # are longer than the longest legal IRC message
+    messages = []
+
+    chunks = len(new_message) / max_length
+    if len(new_message) % max_length != 0:
+        chunks += 1
+
+    for i in range(chunks):
+        chunk = new_message[max_length * i:max_length * (i + 1)]
+        messages.append(build_message(chunk))
+
+    return "\n".join(messages)
 
 weechat.hook_modifier("irc_out_privmsg", "out_modifier", "")
